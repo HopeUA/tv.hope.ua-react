86f44abac3c749df2382ca0f60d69ec3bcbaabd0
AntonGarasko <tonius7570@gmail.com>
2016-06-20 17:32:40 +0300

    add component Header

---
+++ src/components/Shared/Header/component.tpl.js
@@ -0,0 +1,210 @@
+import React, { PropTypes } from 'react';
+import Styles from './main.scss';
+import PixelPerfect from 'components/PixelPerfect/component';
+import BreakPoints from 'components/PixelPerfect/breakpoints';
+import InlineSvg from 'components/InlineSvg/component';
+
+import SvgLogo from 'components/Shared/Header/Assets/logo.svg';
+import SvgMenuHamburger from 'components/Shared/Header/Assets/menuHamburger.svg';
+import SvgSocialFacebook from 'components/Shared/Header/Assets/socialFacebook.svg';
+import SvgSocialInstagram from 'components/Shared/Header/Assets/socialInstagram.svg';
+import SvgSocialOk from 'components/Shared/Header/Assets/socialOk.svg';
+import SvgSocialTwitter from 'components/Shared/Header/Assets/socialTwitter.svg';
+import SvgSocialVk from 'components/Shared/Header/Assets/socialVk.svg';
+import SvgSocialYoutube from 'components/Shared/Header/Assets/socialYoutube.svg';
+import SvgWorldwide from 'components/Shared/Header/Assets/worldwide.svg';
+import SvgWorTopFb from 'components/Shared/Header/Assets/topFb.svg';
+import SvgWorTopTwitter from 'components/Shared/Header/Assets/topTwitter.svg';
+import SvgWorTopVk from 'components/Shared/Header/Assets/topVk.svg';
+import SvgWorTopYoutube from 'components/Shared/Header/Assets/topYoutube.svg';
+import SvgLangArrow from 'components/Shared/Header/Assets/langArrow.svg';
+
+export default function Header(props) {
+    const templates = [
+        BreakPoints.phonePortrait.name,
+        BreakPoints.phoneLandscape.name,
+        BreakPoints.tabletPortrait.name,
+        BreakPoints.tabletLandscape.name,
+        BreakPoints.desktop.name
+    ];
+
+    const { mediaType } =  props;
+
+    return (
+        <PixelPerfect templates={ templates } component="header">
+            <section className={ Styles.headerComponent }>
+                { [
+                    BreakPoints.phonePortrait.name,
+                    BreakPoints.phoneLandscape.name
+                ].indexOf(mediaType) !== -1 ? (
+                    <div>
+                        <header>
+                            <a className={ Styles.logo }>
+                                <InlineSvg content={ SvgLogo }/>
+                            </a>
+                            <a className={ Styles.menu }>
+                                <InlineSvg content={ SvgMenuHamburger }/>
+                            </a>
+                        </header>
+                        <div className={ Styles.wrap }>
+                            <ul className={ Styles.main }>
+                                <li><a href="#">Все программы</a></li>
+                                <li><a href="#">ТВ Программа</a></li>
+                                <li className={ Styles.donation }>
+                                    <a href="#">Пожертвовать</a>
+                                </li>
+                                <li><a href="#">ТВ Онлайн</a></li>
+                            </ul>
+                            <ul className={ Styles.sub }>
+                                <li><a href="#">Новости</a></li>
+                                <li><a href="#">Где нас смотреть?</a></li>
+                                <li><a href="#">О Телеканале</a></li>
+                                <li><a href="#">Контакты</a></li>
+                            </ul>
+                            <div className={ Styles.social }>
+                                <a href="#" className={ Styles.youtube }>
+                                    <InlineSvg content={ SvgSocialYoutube }/>
+                                </a>
+                                <a href="#"className={ Styles.instagram } >
+                                    <InlineSvg content={ SvgSocialInstagram }/>
+                                </a>
+                                <a href="#" className={ Styles.twitter }>
+                                    <InlineSvg content={ SvgSocialTwitter }/>
+                                </a>
+                                <a href="#"className={ Styles.vk } >
+                                    <InlineSvg content={ SvgSocialVk }/>
+                                </a>
+                                <a href="#" className={ Styles.fb }>
+                                    <InlineSvg content={ SvgSocialFacebook }/>
+                                </a>
+                                <a href="#" className={ Styles.ok }>
+                                    <InlineSvg content={ SvgSocialOk }/>
+                                </a>
+                            </div>
+                            <div className={ Styles.languages }>
+                                <span>Язык сайта:</span>
+                                <a href="#" className={ Styles.ru }>Русский</a>
+                                <a href="#" className={ Styles.ua }>Украинский</a>
+                            </div>
+                        </div>
+                    </div>
+
+                ) : null }
+
+                { [
+                    BreakPoints.tabletPortrait.name
+                ].indexOf(mediaType) !== -1 ? (
+                    <div>
+                        <header>
+                            <a className={ Styles.logo }>
+                                <InlineSvg content={ SvgLogo }/>
+                            </a>
+                            <ul className={ Styles.main }>
+                                <li><a href="#">Все программы</a></li>
+                                <li><a href="#">ТВ Программа</a></li>
+                                <li><a href="#">ТВ Онлайн</a></li>
+                            </ul>
+                            <a className={ Styles.menu }>
+                                <InlineSvg content={ SvgMenuHamburger }/>
+                            </a>
+                        </header>
+                        <div className={ Styles.wrap }>
+                            <ul className={ Styles.sub }>
+                                <li><a href="#">Где нас смотреть?</a></li>
+                                <li><a href="#">Новости</a></li>
+                                <li><a href="#">Пожертвовать</a></li>
+                                <li><a href="#">О Телеканале</a></li>
+                                <li><a href="#">Контакты</a></li>
+                            </ul>
+                            <div className={ Styles.social }>
+                                <a href="#" className={ Styles.youtube }>
+                                    <InlineSvg content={ SvgSocialYoutube }/>
+                                </a>
+                                <a href="#"className={ Styles.instagram } >
+                                    <InlineSvg content={ SvgSocialInstagram }/>
+                                </a>
+                                <a href="#" className={ Styles.twitter }>
+                                    <InlineSvg content={ SvgSocialTwitter }/>
+                                </a>
+                                <a href="#"className={ Styles.vk } >
+                                    <InlineSvg content={ SvgSocialVk }/>
+                                </a>
+                                <a href="#" className={ Styles.fb }>
+                                    <InlineSvg content={ SvgSocialFacebook }/>
+                                </a>
+                                <a href="#" className={ Styles.ok }>
+                                    <InlineSvg content={ SvgSocialOk }/>
+                                </a>
+                            </div>
+                            <div className={ Styles.languages }>
+                                <span>Язык сайта:</span>
+                                <a href="#" className={ Styles.ru }>Русский</a>
+                                <a href="#" className={ Styles.ua }>Украинский</a>
+                            </div>
+                        </div>
+                    </div>
+                ) : null }
+
+                { [
+                    BreakPoints.tabletLandscape.name,
+                    BreakPoints.desktop.name,
+                    BreakPoints.desktopWide.name,
+                    BreakPoints.desktopHD.name,
+                    BreakPoints.desktopMega.name
+                ].indexOf(mediaType) !== -1 ? (
+                    <div>
+                        <header>
+                            <a href="#" className={ Styles.world }>
+                                <InlineSvg content={ SvgWorldwide }/>
+                                Всемирный HopeChannel
+                            </a>
+                            <ul className={ Styles.sub }>
+                                <li>Где нас <a href="#"> <span>смотреть</span> <span>&#8594;</span></a></li>
+                                <li><a href="#">О Телеканале</a></li>
+                                <li><a href="#">Изучение Библии</a></li>
+                                <li><a href="#">Новости</a></li>
+                                <li><a href="#">Контакты</a></li>
+                            </ul>
+                            <div className={ Styles.social }>
+                                <a href="#" className={ Styles.youtube }>
+                                    <InlineSvg content={ SvgWorTopYoutube }/>
+                                </a>
+                                <a href="#" className={ Styles.twitter }>
+                                    <InlineSvg content={ SvgWorTopTwitter }/>
+                                </a>
+                                <a href="#"className={ Styles.vk } >
+                                    <InlineSvg content={ SvgWorTopVk }/>
+                                </a>
+                                <a href="#" className={ Styles.fb }>
+                                    <InlineSvg content={ SvgWorTopFb }/>
+                                </a>
+                            </div>
+                        </header>
+                        <div className={ Styles.wrap }>
+                            <a className={ Styles.logo }>
+                                <InlineSvg content={ SvgLogo }/>
+                            </a>
+                            <ul className={ Styles.main }>
+                                <li><a href="#">Все программы</a></li>
+                                <li><a href="#">ТВ Программа</a></li>
+                                <li><a href="#">Пожертвовать</a></li>
+                                <li><a href="#">ТВ Онлайн</a></li>
+                            </ul>
+                            <div className={ Styles.languages }>
+                                <a href="#" className={ Styles.ru }></a>
+                                <InlineSvg className={ Styles.topArrow } content={ SvgLangArrow }/>
+                                <InlineSvg className={ Styles.bottomArrow } content={ SvgLangArrow }/>
+                                <a href="#" className={ Styles.ua }></a>
+                            </div>
+                        </div>
+                    </div>
+                ) : null }
+            </section>
+        </PixelPerfect>
+    );
+}
+
+Header.propTypes = {
+    mediaType: PropTypes.string.isRequired
+};
+
--- src/components/Shared/index.js
+++ src/components/Shared/index.js
@@ -1,2 +1,3 @@
 export PageHead from './PageHead/component';
 export Divider from './Divider/component.js';
+export Header from './Header/component.tpl';
