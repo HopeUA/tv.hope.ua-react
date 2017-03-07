9d0a46fb44dc682e2111e887b62d4b7de80cccf4
Korovchenko <jenakorj@gmail.com>
2017-03-07 11:03:22 +0200

    removed local end point

--- src/api/Content.js
+++ src/api/Content.js
@@ -10,11 +10,10 @@ class Content {
     static endpoint = 'https://tv.hope.ua/api';
 
     static async getArticles(params) {
-        const localEndpoint = 'https://tv.hope.ua/api';
         const resource = 'articles';
         const limit = 'limit=15';
 
-        const url = `${localEndpoint}/${resource}.json?category=${params.type}&${limit}`;
+        const url = `${this.endpoint}/${resource}.json?category=${params.type}&${limit}`;
 
         const response = await fetch(url);
         const result = await response.json();
