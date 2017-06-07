1f7ce6062fc08cd25c537058cc5f440feb529632
Korovchenko <jenakorj@gmail.com>
2017-06-07 11:23:46 +0300

    fix comments

--- src/components/Show/About/View/Common/index.js
+++ src/components/Show/About/View/Common/index.js
@@ -9,11 +9,27 @@ import React from 'react';
  * Style Import
  */
 import Styles from './Styles/main.scss';
+import Grid from 'theme/Grid.scss';
 
 function Common() {
     return (
-        <section className={ Styles.aboutComponent }>
-            index
+        <section className={ Grid.container }>
+            <section className={ Styles.aboutComponent }>
+                <h1 className={ Styles.title }>О программе</h1>
+                <div className={ Styles.content }>
+                    <div className={ Styles.image }/>
+                    <div className={ Styles.text }>
+                        <h2 className={ Styles.showName }>Хліб щоденний</h2>
+                        <p className={ Styles.description }>
+                            На следующее утро с болью в сердце пророк вышел, чтобы встретить заблуждающегося царя.
+                            Самуил лелеял в сердце надежду, что, одумавшись, Саул признает свой грех, раскается,
+                            смирится и вновь обретет Божественную благосклонность. Но когда сделан первый шаг по стезе
+                            беззакония, остальной путь становится легким. Развращенный своим непослушанием, Саул вышел
+                            встречать Самуила с ложью на устах.
+                        </p>
+                    </div>
+                </div>
+            </section>
         </section>
     );
 }
